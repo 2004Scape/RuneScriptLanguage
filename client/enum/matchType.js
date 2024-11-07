994c915224c3a24dcc90bdb58af1015dc21c0f50
klymp <14829626+kylmp@users.noreply.github.com>
2024-11-07 16:10:56 +0700

    feat: Add goto definitions for var, constant, proc, label, queue, timer

---
+++ client/enum/matchType.js
@@ -0,0 +1,30 @@
+const matchType = {
+  UNKNOWN: {id: 'UNKNOWN'},
+  LOCAL_VAR: {id: 'LOCAL_VAR'},
+  GLOBAL_VAR: {id: 'GLOBAL_VAR', definitionFiles: ["varn", "varp", "varns"], definitionFormat: '[NAME]'},
+  CONSTANT: {id: 'CONSTANT', definitionFiles: ["constant"], definitionFormat: '^NAME'},
+  LABEL: {id: 'LABEL', definitionFiles: ["rs2"], definitionFormat: '[label,NAME]'},
+  PROC: {id: 'PROC', definitionFiles: ["rs2"], definitionFormat: '[proc,NAME]'},
+  TIMER: {id: 'TIMER', definitionFiles: ["rs2"], definitionFormat: '[timer,NAME]'},
+  SOFTTIMER: {id: 'SOFTTIMER', definitionFiles: ["rs2"], definitionFormat: '[softtimer,NAME]'},
+  QUEUE: {id: 'QUEUE', definitionFiles: ["rs2"], definitionFormat: '[queue,NAME]'},
+  LOCAL_VAR_DECLARATION: {id: 'LOCAL_VAR_DECLARATION'},
+  GLOBAL_VAR_DECLARATION: {id: 'GLOBAL_VAR_DECLARATION'},
+  CONSTANT_DECLARATION: {id: 'CONSTANT_DECLARATION'},
+  LABEL_DECLARATION: {id: 'LABEL_DECLARATION'},
+  PROC_DECLARATION: {id: 'PROC_DECLARATION'},
+  OBJ_DECLARATION: {id: 'OBJ_DECLARATION'},
+  NPC_DECLARATION: {id: 'NPC_DECLARATION'},
+  LOC_DECLARATION: {id: 'LOC_DECLARATION'},
+  PARAM_DECLARATION: {id: 'PARAM_DECLARATION'},
+  SEQ_DECLARATION: {id: 'SEQ_DECLARATION'},
+  STRUCT_DECLARATION: {id: 'STRUCT_DECLARATION'},
+  DBROW_DECLARATION: {id: 'DBROW_DECLARATION'},
+  DBTABLE_DECLARATION: {id: 'DBTABLE_DECLARATION'},
+  ENUM_DECLARATION: {id: 'ENUM_DECLARATION'},
+  TIMER_DECLARATION: {id: 'TIMER_DECLARATION'},
+  SOFTTIMER_DECLARATION: {id: 'SOFTTIMER_DECLARATION'},
+  QUEUE_DECLARATION: {id: 'QUEUE_DECLARATION'}
+};
+
+module.exports = matchType;
