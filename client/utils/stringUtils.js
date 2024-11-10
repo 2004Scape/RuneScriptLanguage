07afd3d41c6c4ec56d5d2f9593be082452fc609b
Kyle <14829626+kylmp@users.noreply.github.com>
2024-11-11 06:50:03 +0700

    fix: nested params matching (#13)
    * fix: nested params matching
    
    * add to gitignore

--- client/utils/stringUtils.js
+++ client/utils/stringUtils.js
@@ -9,4 +9,23 @@ const getLines = function(input) {
   return input.split(eolRegex);
 }
 
-module.exports = { getLineText, getLines };
+const nthIndexOf = function(input, pattern, n) {
+  let i = -1;
+  while (n-- > 0 && i++ < input.length) {
+    i = input.indexOf(pattern, i);
+    if (i < 0) break;
+  }
+  return i;
+}
+
+const truncateMatchingParenthesis = function(str) {
+  let truncateIndex = 0;
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === '(') count++;
+    if (str.charAt(i) === ')' && --count === 0) truncateIndex = i;
+  }
+  return str.substring(truncateIndex + 1);
+}
+
+module.exports = { getLineText, getLines, nthIndexOf, truncateMatchingParenthesis };
