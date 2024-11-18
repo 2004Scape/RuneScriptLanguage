601d27b0cecf483bd0aeebc81374ad3bf4f7061f
klymp <14829626+kylmp@users.noreply.github.com>
2024-11-18 13:57:19 +0300

    saved

--- client/resource/configTags.js
+++ client/resource/configTags.js
@@ -4,13 +4,12 @@ const configTags = {
   PARAM: build(matchType.PARAM),
   TABLE: build(matchType.DBTABLE),
   HUNTMODE: build(matchType.HUNT),
-  ANIM: build(matchType.SEQ),
-  READYANIM: build(matchType.SEQ),
-  WALKANIM: build(matchType.SEQ),
 }
 
+// the tag will be converted toUpperCase, so consider that when making the regex
 const regexTags = [
   build(matchType.OBJ, ["inv"], /STOCK\d+/),
+  build(matchType.SEQ, ["loc", "npc", "if", "spotanim"], /\w*ANIM\w*/),
 ]
 
 function build(match, fileTypes = [], regex) {
